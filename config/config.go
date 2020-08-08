a8323d57d11f582d3e2499ab2f9ba06ee564db54
Farhaan Bukhsh <farhaan.bukhsh@gmail.com>
2020-08-08 16:36:53 +0530

    Refactored config read from file function
    Signed-off-by: Farhaan Bukhsh <farhaan.bukhsh@gmail.com>

--- config/config.go
+++ config/config.go
@@ -1,8 +1,28 @@
 package config
 
+import (
+	"encoding/json"
+	"io/ioutil"
+	"log"
+)
+
 // Config loads and stores the configuration
 type Config struct {
 	Hostname string `json:"hostname"`
 	Database string `json:"database"`
 	Port     string `json:"port"`
+}
+
+// ReadConfig function helps to get the config back
+func ReadConfig(configFile string) Config {
+	var cfg Config
+	fileData, err := ioutil.ReadFile(configFile)
+	if err != nil {
+		log.Fatalf("Not able to read file")
+	}
+	err = json.Unmarshal(fileData, &cfg)
+	if err != nil {
+		log.Fatalf("Not able to un marshal the config")
+	}
+	return cfg
 }
